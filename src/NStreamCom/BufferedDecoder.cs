cb284749735c802adc309ae822d9d0bec56bc319
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-12-18 18:15:27 +0200

    Implemented BufferedDecoder with test

---
+++ src/NStreamCom/BufferedDecoder.cs
@@ -0,0 +1,49 @@
+﻿namespace NStreamCom
+{
+    public class BufferedDecoder
+    {
+        private static readonly int BUFFER_CLEAR_SIZE = 512;
+
+        public int DecodedIndex { get; private set; } = 0;
+
+        public byte[] Bytes { get => _bytes.ToArray(); }
+
+        private List<byte> _bytes = new();
+
+        private byte _rightShift = 0;
+
+        public void Reset()
+        {
+            DecodedIndex = 0;
+            _rightShift = 0;
+            if (BUFFER_CLEAR_SIZE <= _bytes.Count)
+                _bytes.Clear();
+        }
+
+        public void Next(byte b, bool isLast)
+        {
+            if (!isLast)
+                EnsureSize()[DecodedIndex] = (byte)(b >> _rightShift);
+
+            if (_rightShift != 0)
+                _bytes[DecodedIndex - 1] |= (byte)(b << (8 -  _rightShift));
+
+            if (_rightShift == 7)
+            {
+                _rightShift = 0;
+            }
+            else
+            {
+                _rightShift++;
+                DecodedIndex++;
+            }
+        }
+
+        private List<byte> EnsureSize()
+        {
+            while (_bytes.Count <= DecodedIndex)
+                _bytes.Add(0);
+            return _bytes;
+        }
+    }
+}
---
+++ tests/BufferedDecoder/Program.cs
@@ -0,0 +1,48 @@
+﻿using NStreamCom;
+using System.Text;
+
+namespace BufferedDecoder
+{
+    public static class Program
+    {
+        private static int RunTest(bool first = false, NStreamCom.BufferedDecoder? decoder = null)
+        {
+            decoder = decoder ?? new();
+
+            const string STR = "Lorem ipsum dolor sit amet, consectetur adipiscing nunc";
+            byte[] encoded = Encoding.UTF8.GetBytes(STR).Encode();
+
+            for (int i = 0; i < encoded.Length; i++)
+                decoder.Next(encoded[i], i == encoded.Length - 1);
+
+            string decoded = Encoding.UTF8.GetString(decoder.Bytes);
+            if (decoded.Length != Sizing.AsDataSize((uint)encoded.Length) || decoded.Length != STR.Length)
+            {
+                Console.WriteLine($"first={first}:Data size mismatch.");
+                return 1;
+            }
+
+            if (decoded != STR)
+            {
+                Console.WriteLine($"first={first}:Encode/Decode data error.");
+                return 2;
+            }
+
+            decoder.Reset();
+            if (first)
+                return RunTest(false, decoder);
+
+            return 0;
+        }
+
+        public static void Main()
+        {
+            int code = RunTest();
+            if (code != 0)
+                Console.WriteLine($"Exit code: {code}");
+            else
+                Console.WriteLine("Success!");
+            Environment.Exit(code);
+        }
+    }
+}
