e3f37112f4ff3755740b6d7af4de2f4c28bde57c
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-12-18 18:41:19 +0200

    Implemented Collector with test

---
+++ Collector/Program.cs
@@ -0,0 +1,72 @@
+﻿using NStreamCom;
+using System.Text;
+
+namespace BufferedDecoder
+{
+    public static class Program
+    {
+        private static int RunTest(bool first = false, NStreamCom.Collector? collector = null)
+        {
+            collector = collector ?? new();
+
+            const string STR = "Lorem ipsum dolor sit amet, consectetur adipiscing nunc";
+            byte[] encoded = Encoding.UTF8.GetBytes(STR).EncodeWithSize();
+
+            if (encoded.Length != STR.Length.AsCollectedSize())
+            {
+                Console.WriteLine("Data as collected size encoded mismatch.");
+                return 1;
+            }
+
+            foreach (byte b in encoded)
+            {
+                collector.Collect(b);
+                if (collector.State == Collector.States.MissingSize)
+                {
+                    Console.WriteLine("Collector error state MissingSize");
+                    return 2;
+                }
+                else if (collector.State == Collector.States.MissingData)
+                {
+                    Console.WriteLine("Collector error state MissingData");
+                    return 2;
+                }
+            }
+
+            if (!collector.DataReady)
+            {
+                Console.WriteLine("Collector data not ready.");
+                return 3;
+            }
+
+            string decoded = Encoding.UTF8.GetString(collector.Data);
+            if (decoded.Length != STR.Length)
+            {
+                Console.WriteLine($"first={first}:Data size mismatch.");
+                return 4;
+            }
+
+            if (decoded != STR)
+            {
+                Console.WriteLine($"first={first}:Encode/Decode data error.");
+                return 5;
+            }
+
+            collector.Reset();
+            if (first)
+                return RunTest(false, collector);
+
+            return 0;
+        }
+
+        public static void Main()
+        {
+            int code = RunTest();
+            if (code != 0)
+                Console.WriteLine($"Exit code: {code}");
+            else
+                Console.WriteLine("Success!");
+            Environment.Exit(code);
+        }
+    }
+}
---
+++ src/NStreamCom/Collector.cs
@@ -0,0 +1,136 @@
+﻿namespace NStreamCom
+{
+    public class Collector
+    {
+        public enum States
+        {
+            Collected,
+            MissingSize,
+            MissingData,
+            WaitingSize,
+            WaitingData
+        }
+
+        public class StateChangedEventArgs : EventArgs
+        {
+            public readonly States PreviousState;
+
+            public readonly States NewState;
+
+            public StateChangedEventArgs(States previousState, States newState)
+            {
+                PreviousState = previousState;
+                NewState = newState;
+            }
+        }
+
+        public delegate void StateChangedEventHandler(object? sender, StateChangedEventArgs e);
+
+        public States State { get; private set; } = States.WaitingSize;
+
+        public bool SizeReady { get => State == States.WaitingData; }
+
+        public bool DataReady { get => State == States.Collected; }
+
+        public uint Size
+        {
+            get
+            {
+                if (!SizeReady)
+                    throw new InvalidOperationException("Size is not ready.");
+                return _nextSize;
+            }
+        }
+
+        public byte[] Data
+        {
+            get
+            {
+                if (!DataReady)
+                    throw new InvalidOperationException("Data is not ready.");
+                return _decoder.Bytes;
+            }
+        }
+
+        public event StateChangedEventHandler? StateChanged;
+
+        private BufferedDecoder _decoder = new();
+
+        private uint _nextSize = 0;
+
+        private States _previousState = States.WaitingSize;
+
+        public void Reset()
+        {
+            _nextSize = 0;
+            _decoder.Reset();
+            State = States.WaitingSize;
+        }
+
+        public States Collect(byte b)
+        {
+            switch (State)
+            {
+                case States.Collected:
+                case States.MissingSize:
+                case States.MissingData:
+                    Reset();
+                    Collect(b);
+                    break;
+                case States.WaitingSize:
+                    CollectSizeByte(b);
+                    break;
+                case States.WaitingData:
+                    CollectDataByte(b);
+                    break;
+                default:
+                    break;
+            }
+
+            if (_previousState != State)
+            {
+                StateChanged?.Invoke(this, new(_previousState, State));
+                _previousState = State;
+            }
+
+            return State;
+        }
+
+        private void CollectSizeByte(byte b)
+        {
+            if ((b & (1 << 7)) == 0)
+            {
+                State = States.MissingSize;
+                return;
+            }
+
+            b &= (byte)~(0xFF << (byte)Sizing.DATA_BITS);
+            bool last = _decoder.DecodedIndex == sizeof(uint);
+            _decoder.Next(b , last);
+
+            if (!last)
+                return;
+
+            _nextSize = BitConverter.ToUInt32(_decoder.Bytes);
+            _decoder.Reset();
+            State = _nextSize == 0 ? States.Collected : States.WaitingData;
+        }
+
+        private void CollectDataByte(byte b)
+        {
+            if ((b & 0b10000000) > 0)
+            {
+                State = States.MissingData;
+                return;
+            }
+
+            bool last = _decoder.DecodedIndex == _nextSize;
+            _decoder.Next(b , last);
+
+            if (!last)
+                return;
+
+            State = States.Collected;
+        }
+    }
+}
