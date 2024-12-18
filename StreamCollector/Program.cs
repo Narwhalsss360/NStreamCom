3f4d38abb8808116db29aad5b8ed15465798797b
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-12-18 18:50:05 +0200

    Implemented StreamCollector with tests

---
+++ StreamCollector/Program.cs
@@ -0,0 +1,59 @@
+﻿using NStreamCom;
+using System.Text;
+
+namespace BufferedDecoder
+{
+    public static class Program
+    {
+        private static int RunTest(bool first = false, NStreamCom.StreamCollector? collectorStream = null)
+        {
+            collectorStream = collectorStream ?? new();
+
+            const string STR = "Lorem ipsum dolor sit amet, consectetur adipiscing nunc";
+            byte[] encoded = Encoding.UTF8.GetBytes(STR).EncodeWithSize();
+
+            if (encoded.Length != STR.Length.AsCollectedSize())
+            {
+                Console.WriteLine("Data as collected size encoded mismatch.");
+                return 1;
+            }
+
+            collectorStream.Write(encoded);
+
+            if (!collectorStream.Collector.DataReady)
+            {
+                Console.WriteLine($"Collector data not ready; {collectorStream.Collector.State}.");
+                return 2;
+            }
+
+            string decoded = Encoding.UTF8.GetString(collectorStream.Collector.Data);
+            if (decoded.Length != STR.Length)
+            {
+                Console.WriteLine($"first={first}:Data size mismatch.");
+                return 3;
+            }
+
+            if (decoded != STR)
+            {
+                Console.WriteLine($"first={first}:Encode/Decode data error.");
+                return 4;
+            }
+
+            collectorStream.Collector.Reset();
+            if (first)
+                return RunTest(false, collectorStream);
+
+            return 0;
+        }
+
+        public static void Main()
+        {
+            int code = RunTest();
+            if (code != 0)
+                Console.WriteLine($"Exit code: {code}");
+            else
+                Console.WriteLine("Success!");
+            Environment.Exit(code);
+        }
+    }
+}
---
+++ src/NStreamCom/StreamCollector.cs
@@ -0,0 +1,49 @@
+﻿namespace NStreamCom
+{
+    public class StreamCollector : Stream
+    {
+        public override bool CanRead => false;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => true;
+
+        public override long Length => Collector.DataReady ? Collector.Size : 0;
+
+        public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
+
+        public Collector Collector { get; set; }
+
+        public bool BreakOnCollectorErrorState { get; set; } = true;
+
+        public StreamCollector(Collector? collector = null)
+        {
+            Collector = collector ?? new();
+        }
+
+        public override void Flush()
+        { }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            for (int i = 0; i < count; i++)
+                if (Collector.Collect(buffer[i + offset]).ErrorState() && BreakOnCollectorErrorState)
+                    break;
+        }
+    }
+}
