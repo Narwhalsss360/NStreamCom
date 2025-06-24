f8e5cb04c7ff82120195609374f473c12808c014
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-06-23 20:10:01 -0400

    Made nsize_int pub, and added encoded_nsize_int, with byte_count sizes

--- src/sizing.zig
+++ src/sizing.zig
@@ -1,8 +1,12 @@
-const nsize_int = u32;
+pub const nsize_int = u32;
 
-const nsize_int_bit_size = 32;
+pub const nsize_int_byte_count = @bitSizeOf(nsize_int) / 8;
 
-const nsize_int_max = (32 + 7 - 1) / 7;
+pub const encoded_nsize_int = u40;
+
+pub const encoded_nsize_int_byte_count = @bitSizeOf(encoded_nsize_int) / 8;
+
+pub const nsize_int_max = (32 + 7 - 1) / 7;
 
 pub fn asTransmissionSize(size: nsize_int) nsize_int {
     return (size * 8 + 7 - 1) / 7;
